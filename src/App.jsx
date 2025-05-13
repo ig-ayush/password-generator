048d4718e959625a9bd4e202f435d5301be6b8d3
Ayush-alt-sys <igayushacharya@gmail.com>
2025-05-13 14:31:15 +0530

    start the UI/UX

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 function App() {
 
   return (
-    <h1 className="bg-orange-400 text-center text-3xl p-2 m-2">Hello coders</h1>
+    <>
+      <h1 className="bg-orange-400 text-center text-3xl p-2 m-2">Hello coders</h1>
+      <h2 className="text-center">Get to start the UI</h2>
+    </>
   )
 }
 
