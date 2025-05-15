6ebb76ac0c19e81a8a885480ab5db1a95dbcba39
Ayush-alt-sys <igayushacharya@gmail.com>
2025-05-15 12:00:02 +0530

    get started the ui in new component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
+import Passw from "./component/PassW"
+
 function App() {
 
   return (
-    <>
-      <h1 className="bg-orange-400 text-center text-3xl p-2 m-2">Hello coders</h1>
-      <h2 className="text-center">Get to start the UI</h2>
-    </>
+    <Passw />
   )
 }
 
---
+++ src/component/PassW.jsx
@@ -0,0 +1,10 @@
+function Passw(){
+    return (
+        <>
+            <h1 className="bg-orange-400 text-center text-3xl p-2 m-2">Hello coders</h1>
+            <h2 className="text-center">Get to start the UI Ok.</h2>
+        </>
+    )
+}
+
+export default Passw;
