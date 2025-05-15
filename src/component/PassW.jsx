2764b2e608a29dcf6cf70b3ad322639169a3dd1b
Ayush-alt-sys <igayushacharya@gmail.com>
2025-05-15 13:04:02 +0530

    started the ui design bsic elements

--- src/component/PassW.jsx
+++ src/component/PassW.jsx
@@ -1,8 +1,28 @@
 function Passw(){
     return (
         <>
-            <h1 className="bg-orange-400 text-center text-3xl p-2 m-2">Hello coders</h1>
-            <h2 className="text-center">Get to start the UI Ok.</h2>
+            <div id="container" className="border p-10 rounded-lg bg-orange-500 ">
+
+                <div 
+                id="password-gen"
+                className="flex items-center gap-10"
+                >
+                    <h1 className="text-2xl font-medium">Genarate your password</h1>
+                    
+                    <div 
+                    id="input-con" 
+                    className=" flex "
+                    >
+                        <input 
+                        type="text"
+                        className="border p-1.5 focus:outline-none"
+                         />
+                        <button 
+                        className="p-1.5 w-[80px] bg-black font-bold text-white hover:bg-blue-950 duration-150"
+                        >Copy</button>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
